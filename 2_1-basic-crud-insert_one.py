deca52f260c879f333a4f42d52c45130dad6e65e
Wiam Nasr <wiamnasr@gmail.com>
2022-05-07 19:57:52 +0100

    organising notes on CRUD operations with pymongo

--- 2_1-basic-crud-insert_one.py
+++ 2_1-basic-crud-insert_one.py
@@ -12,6 +12,10 @@ mydict = { "name": "John", "address": "Highway 37"}
 
 x = customerscollection.insert_one(mydict)
 
+print("Customers Collection After Inserting one:")
+for customer in customerscollection.find():
+    print(customer)
+
 
 
 # Another Example using insert_many
@@ -24,21 +28,33 @@ mylist = [{"first_name": "Wiam", "last_name": "Nasr", "age": 31, "history": [{"d
 
 y = patientscollection.insert_many(mylist)
 
-# print(mydb)
-print(patientscollection.find())
+print("Patients collection after inserting many patients with their details from mylist: ")
+for patient in patientscollection.find():
+    print(patient)
 
 
 # Updating the patients collection in the hospital database, where the first name is Wiam, changing the name, age and history to new values
 # For now overwriting the old history list with a new list
 updatedPatientsCollection = patientscollection.update_one({"first_name": "Wiam"}, {"$set": {"last_name":"Nasrr", "age": 31.3, "history": [{"disease": "headache", "treatment": 54}]}})
 
-print(updatedPatientsCollection)
+print("Updated patients collection: ")
+for patient in patientscollection.find():
+    print(patient)
 
 
 # Finding all patients older than 30:
 olderthan30 = patientscollection.find({"age": {"$gt": 30}})
 
-print(olderthan30)
+print("Patients who are older than thirty years old: ")
+for patient in patientscollection.find():
+    print(patient)
 
 # Deleting all the patients who have had a cold
 patientswithcold = patientscollection.delete_many({"history.disease": "cold"})
+
+# Dropping mydatabases from earlier
+myclient.drop_database(mydb)
+myclient.drop_database(hospitaldatabase)
+
+print("list of remaining databases after deleting the above database")
+print(myclient.list_database_names())
